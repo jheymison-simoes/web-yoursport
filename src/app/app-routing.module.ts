d7209582f4285ad9affd4f2a51dedc57be9fcc51
Jheymison Simões <jheymison.santos@mottu.com.br>
2022-02-28 20:04:24 -0300

    Adicionado compnente not found para rotas não encontradas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomePageComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomePageComponent}
+  { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
